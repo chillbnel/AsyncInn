5f44b96205966b8c75cf5a47f04800a6a9781be8
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-04 19:22:03 -0800

    Interfaces drafted

---
+++ AsyncInn/Models/Interfaces/IRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    interface IRoom
+    {
+        //Creating
+        Task CreateHotel(Hotel hotel);
+
+        //Updating
+        Task UpdateHotel(Hotel hotel);
+
+        //Deleting
+        Task DeleteHotel(int id);
+
+        //Reading
+        Task<List<Hotel>> GetHotels();
+
+        Task<Hotel> GetHotel(int? id);
+    }
+}
